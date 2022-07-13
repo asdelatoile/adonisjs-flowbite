ff25b642af6cba4e5f44d95b441bff13e4274e63
asdelatoile <andy@asdelatoile.com>
2022-07-13 17:47:31 +0200

    Styles

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # adonisjs-flowbite  
 starter with adonisjs/flowbite
+
+https://windmill-dashboard.vercel.app/forms.html
+https://docs.adonisjs.com/guides/introduction
+https://tailwindcss.com/docs/installation
+https://flowbite.com/docs/getting-started/introduction/
--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,27 +1,31 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import LoginValidator from 'App/Validators/LoginValidator'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import LoginValidator from "App/Validators/LoginValidator";
 
 export default class AuthController {
-
     public async login({ view }: HttpContextContract) {
-        return view.render('login')
+        return view.render("login");
     }
 
-    public async authenticate({ request, auth, session, response }: HttpContextContract) {
-        await request.validate(LoginValidator)
+    public async authenticate({
+        request,
+        auth,
+        session,
+        response,
+    }: HttpContextContract) {
 
-        const email = request.input('email')
-        const password = request.input('password')
+        const email = request.input("email");
+        const password = request.input("password");
+
+        await request.validate(LoginValidator);
 
         try {
-            await auth.use('web').attempt(email, password)
-            session.flash('success', 'Ok !')
-            // return response.redirect().back()
-            return response.redirect().toRoute('permissions.index')
+            await auth.use("web").attempt(email, password);
+            session.flash("success", "Ok !");
+            return response.redirect().toRoute("permissions.index");
         } catch {
-            session.flash('email', email)
-            session.flash('error', 'Error !')
-            return response.redirect().back()
+            session.flash("email", email);
+            session.flash("error", "Error !");
+            return response.redirect().back();
         }
     }
 }
